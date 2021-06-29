20ac936b31d5415634174d2896dbf119cd55564c
revbase28 <reihanreinaldimctna@gmail.com>
2021-06-29 07:48:48 +0700

    test get url origin

--- findyouroutfit/Doc/js/searchEvent.js
+++ findyouroutfit/Doc/js/searchEvent.js
@@ -2,5 +2,7 @@ var searchButton = document.getElementById("searchButton");
 var keywordInput = [];
 var dummyArray = ["Apple", "Pinapple", "Pen"];
 searchButton.onclick = function() {
-    keywordInput = document.getElementById("searchKeywordInput").value.split(" ")
+    let base_url = window.location.origin
+    keywordInput = document.getElementById("searchKeywordInput")
+    alert(base_url)
 }
