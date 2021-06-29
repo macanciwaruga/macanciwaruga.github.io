e747e32d0696d83834ca93a102944ed6221612d5
revbase28 <reihanreinaldimctna@gmail.com>
2021-06-29 11:54:32 +0700

    Create grid gallery in find category page for all data

---
+++ findyouroutfit/Doc/js/responseSearchEvent.js
@@ -0,0 +1,43 @@
+window.onload = function() {
+    showImages()
+    let url = document.location.href
+    console.info(url.split('?').length)
+    if(url.split('?').length > 1) {
+        let params = url.split('?')[1].split('=')[1].split("%20")
+        let param = "";
+        for (let item of params) {
+            param += item + " "
+        }
+        document.getElementById("searchKeywordInput").value = param
+    }
+}
+
+function showImages() {
+    var obj = getJsonObject()
+    var images = []
+    var container = document.getElementById("item-container")
+    for (let item of obj) {
+        //container.innerHTML = '<div class="grid-item"><img src="assets/img/outfit/' + item.file_upload + '"/></div>'
+        var div_item = document.createElement("div")
+        div_item.className = "grid-item"
+        div_item.innerHTML = '<img src="assets/img/outfit/' + item.file_upload + '"/>'
+        container.appendChild(div_item)
+        images.push(item.file_upload)
+    }
+
+    const grid = document.querySelector(".grid")
+    const mansonry = new Masonry(grid, {
+        itemSelector: '.grid-item', 
+        gutter: 20, 
+    })
+
+    document.getElementById("data-count").innerHTML = obj.length + " Data Found"
+
+}
+
+function getJsonObject() {
+    var request = new XMLHttpRequest();
+    request.open("GET", "json/outfit_labeled.json", false);
+    request.send(null);
+    return JSON.parse(request.responseText);
+}
--- findyouroutfit/Doc/js/searchEvent.js
+++ findyouroutfit/Doc/js/searchEvent.js
@@ -5,7 +5,7 @@ searchButton.onclick = function() {
     let base_url = window.location.origin
     keywordInput = document.getElementById("searchKeywordInput").value
 
-    let url = base_url + "/Web%20Profile%20Kelompok/findyouroutfit/find_category.html?q=" + encodeURIComponent(keywordInput)
+    let url = base_url + "/findyouroutfit/find_category.html?q=" + encodeURIComponent(keywordInput)
     if(keywordInput !== ""){
         window.open(url, '_self')
     }
