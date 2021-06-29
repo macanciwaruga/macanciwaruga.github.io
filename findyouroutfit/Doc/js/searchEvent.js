ac74ad9fbf6ca55446b414204d65d39c46126f93
revbase28 <reihanreinaldimctna@gmail.com>
2021-06-29 09:11:33 +0700

    transfer data to another page

--- findyouroutfit/Doc/js/searchEvent.js
+++ findyouroutfit/Doc/js/searchEvent.js
@@ -3,6 +3,10 @@ var keywordInput = [];
 var dummyArray = ["Apple", "Pinapple", "Pen"];
 searchButton.onclick = function() {
     let base_url = window.location.origin
-    keywordInput = document.getElementById("searchKeywordInput")
-    alert(base_url)
+    keywordInput = document.getElementById("searchKeywordInput").value
+
+    let url = base_url + "/Web%20Profile%20Kelompok/findyouroutfit/find_category.html?q=" + encodeURIComponent(keywordInput)
+    if(keywordInput !== ""){
+        window.open(url, '_self')
+    }
 }
